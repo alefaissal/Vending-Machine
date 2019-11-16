e6abcf790c7ca1577f5788a15cc503187d70e185
Alex Faissal <alefaissal@gmail.com>
2019-11-16 12:57:08 -0800

    Pass 9 tests

--- __test__/VendingMachine.spec.js
+++ __test__/VendingMachine.spec.js
@@ -69,33 +69,7 @@ describe("Vending Machine without product", () => {
   });
 });
 
-// describe("Vending Machine without coins", () => {
-//   beforeEach(() => {
-//     newInventory = inventory;
-//     //for id = "A1"
-//     newInventory.items[0].current = 0;
-//     //for id ="B1"
-//     newInventory.items[4].current = 0;
-//     vendingMachine = new VendingMachine(newInventory);
-//   });
-
-//   describe("When user input order and there is no product left", () => {
-//     it("should return No product available, and return money", () => {
-//       expect(() => vendingMachine.order("A1", 2.0)).toThrowError(
-//         "Doritos Nachos is not available, get your 2.0 dolars back"
-//       );
-//     });
-//   });
-//   describe("When user input order and there is no product left", () => {
-//     it("should return No product available, and return money", () => {
-//       expect(() => vendingMachine.order("B1", 3.0)).toThrowError(
-//         "Ruffles Regular is not available, get your 3.0 dolars back"
-//       );
-//     });
-//   });
-// });
-
-describe("Refill Vending Machine", () => {
+describe("Refill items in Vending Machine", () => {
   inventoryEmpty = inventory;
   inventoryEmpty.items[2].current = 0;
   inventoryEmpty.items[3].current = 0;
@@ -104,6 +78,19 @@ describe("Refill Vending Machine", () => {
   describe("When user called this function ", () => {
     it("should refill the vending machine with products", () => {
       expect(vendingMachine.refilInventory(inventoryEmpty)).toBe(inventory);
+    });
+  });
+});
+
+describe("Refill coins Vending Machine", () => {
+  inventoryEmpty = inventory;
+  inventoryEmpty.coins[0].current = 0;
+  inventoryEmpty.coins[2].current = 0;
+  inventoryEmpty.coins[3].current = 0;
+
+  describe("When user called this function ", () => {
+    it("should refill the vending machine with coins", () => {
+      expect(vendingMachine.refilCoins(inventoryEmpty)).toBe(inventory);
     });
   });
 });
